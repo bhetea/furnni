a7c7f0015937e9fb28526030bb38031ad68b4be0
Betty Polycarp <bhetea001@gmail.com>
2024-07-12 11:59:09 +0100

    Create the home page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+<x-guest-layout>
+    <nav arial-label="Furni navigation bar" class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark">
+
+        <div class="container">
+            <a class="navbar-brand" href="index.html">Furni<span>.</span>
+            </a>
+
+            <button aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"
+                data-bs-target="#navbarsFurni" data-bs-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="navbar-collapse collapse" id="navbarsFurni">
+                <ul class="custom-navbar-nav navbar-nav mb-md-0 mb-2 ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li><a class="nav-link" href="shop.html">Shop</a></li>
+                    <li><a class="nav-link" href="about.html">About us</a></li>
+                    <li><a class="nav-link" href="services.html">Services</a></li>
+                    <li class="active"><a class="nav-link" href="blog.html">Blog</a></li>
+                    <li><a class="nav-link" href="contact.html">Contact us</a></li>
+                </ul>
+
+                <ul class="custom-navbar-cta navbar-nav mb-md-0 mb-2 ms-5">
+                    <li><a class="nav-link" href="#"><img src="assets/images/user.svg"></a></li>
+                    <li><a class="nav-link" href="cart.html"><img src="assets/images/cart.svg"></a></li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+</x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta content="{{ csrf_token() }}" name="csrf-token">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Ecommerce') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net" rel="preconnect">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Bootstrap CSS -->
+        <link href="{{ asset('') }}assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+        <link href="{{ asset('') }}assets/css/tiny-slider.css" rel="stylesheet">
+        <link href="{{ asset('') }}assets/css/style.css" rel="stylesheet">
+
+        <!--JavaScript-->
+        <script src="{{ asset('') }}assets/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('') }}assets/js/tiny-slider.js"></script>
+        <script src="{{ asset('') }}assets/js/custom.js"></script>
+
+        @stack('css')
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
+    <body>
+        @stack('js.body')
+
+        {{ $slot }}
+
+        <!--JavaScript-->
+        <script src="{{ asset('') }}assets/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('') }}assets/js/tiny-slider.js"></script>
+        <script src="{{ asset('') }}assets/js/custom.js"></script>
+
+        @stack('js')
     </body>
+
 </html>
